8c60117bc163a053b38141b422f5c5f1b22ac08a
Renan Pallin <renanpallin@gmail.com>
2017-12-06 12:43:09 -0200

    Adjusting CSS and swiitch component

--- src/App.js
+++ src/App.js
@@ -2,17 +2,24 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Switch from 'react-switch';
+
 class App extends Component {
+    state = { checked: false };
     render() {
         return (
             <div className="App">
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
                 </header>
                 <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to
-                    reload.
+                    <div className="flex-center">
+                        <Switch
+                            checked={this.state.checked}
+                            onChange={checked => this.setState({ checked })}
+                            id="normal-switch"
+                        />
+                    </div>
                 </p>
             </div>
         );
